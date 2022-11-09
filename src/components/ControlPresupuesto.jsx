cf4ef23de6d3a79dca539dcd52750998f43add63
adisonsud <adison.chire.broncano@gmail.com>
2022-11-08 20:02:09 -0500

    IMAGEN

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -1,19 +1,31 @@
 import { useState ,useEffect } from "react"
+import {CircularProgressbar, buildStyles} from 'react-circular-progressbar' /* imagen */
+import "react-circular-progressbar/dist/styles.css" /* imagen */
 
 const ControlPresupuesto = ({gastos, presupuesto}) => {
 
+  const [porcentaje, setPorcentaje] = useState(0)
   const [disponible,setDisponible] = useState(0)
   const [gastado,setGastado] = useState(0)
 
-
+  /* ----------------------------------------------------------------------------- */
   useEffect(() => {
     const totalGastado = gastos.reduce((total, gasto) => gasto.cantidad + total, 0 )
     setGastado(totalGastado);
-
+    
     const totalDisponible = presupuesto - totalGastado
     setDisponible(totalDisponible)
+    
+    /* calcular el porcentaje gastado */
+    const nuevoPorcentaje = (((presupuesto - totalDisponible) / presupuesto) * 100).toFixed(2) /* .toFixed retorna la cantidad de digitos despues del coma */
+    setTimeout(() => {
+      
+      setPorcentaje(nuevoPorcentaje);
+    }, 800);
+
   }, [gastos])
   
+  /* ----------------------------------------------------------------------------- */
   
   const formatearCantidad = (cantidad) => {
     /* formatea el monto ingresado en dolares y con 2 decimakes => $300.00 */
@@ -27,7 +39,12 @@ const ControlPresupuesto = ({gastos, presupuesto}) => {
     <div className='contenedor-presupuesto contenedor sombra dos-columnas'>
         
         <div>
-            <p>Grafica aqui</p>
+            {/* Grafica aqui */}
+            <CircularProgressbar
+              styles={buildStyles({})}
+              value={porcentaje}
+            />
+
         </div>
 
         <div className='contenido-presupuesto'>
